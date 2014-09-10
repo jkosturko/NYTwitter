bd84e4e5ec3ae50a8847ef77d40c7e03bc17fb3c
jkosturko <jessica.kosturko@gmail.com>
2014-09-10 11:31:54 -0400

    backing up work computer

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'gPCW3YmfNMOMAFyd8TNeyQ');
+define('CONSUMER_SECRET', 'U5hFyZZyZdsN4kLEJg2bLhvEUojbQcaOwnbiLSyarik');
+define('OAUTH_CALLBACK', 'http://nytwitter.jessicakosturkodev.com/~205180/NYTwitter/callback.php');
